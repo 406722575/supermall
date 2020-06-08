765401c6293dcb5dbeaa557db540e8779c87ef96
asus <40672255>
2020-06-08 22:32:31 +0800

    添加本周推荐

---
+++ src/views/home/childComps/FeatureView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="feature">
+        <a href="https://act.mogu.com/sjh0011?ptp=32._mf1_1239_4537.0.0.Jqg1x9a3&acm=3.mce.1_10_1oaco.140653.0.yd34ns1kvy6bj.
+        pos_0-m_566552-sd_119-mf_4537_1211938-idx_0-mfs_15-dm1_5000">
+            <img src="~assets/img/home/recommend_bg.jpg" alt="">
+        </a>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "FeatureView"
+  }
+</script>
+
+<style scoped>
+    .feature img {
+        width: 100%;
+    }
+</style>
