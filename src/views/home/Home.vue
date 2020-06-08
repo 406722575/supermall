b886e6422605ba958f6c0554c0226c978dc29373
asus <40672255>
2020-06-08 22:29:50 +0800

    添加本周推荐

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,50 +1,61 @@
 <template>
-  <div id="home">
-    <nav-bar class="home-nav">
-      <div class="nav-center" slot="nav-center">购物街</div>
-    </nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
-    <recommend-view :recommends="recommends"/>
-  </div>
+    <div id="home">
+        <nav-bar class="home-nav">
+            <div class="nav-center" slot="nav-center">购物街</div>
+        </nav-bar>
+        <home-swiper :banners="banners"/>
+        <recommend-view :recommends="recommends"/>
+        <feature-view/>
+    </div>
 </template>
 
 <script>
-import NavBar from "components/common/navbar/NavBar";
-import HomeSwiper from './childComps/HomeSwiper'
-import RecommendView from './childComps/RecommendView'
-import {getHomeMultidata} from 'network/api/home'
-export default {
-  name: "Home",
-  components: {
-    NavBar,
-    HomeSwiper,
-    RecommendView,
-  },
-  data() {
-    return {
-      banners: [],
-      recommends: []
-    }
-  },
-  created() {
-    getHomeMultidata().then(res => {
-      console.log(res)
-      this.banners = res.banner.list
-      // console.log(this.banners)
-      this.recommends = res.recommend.list
-    })
-  },
-  methods: {
+  import NavBar from "components/common/navbar/NavBar";
+  import HomeSwiper from './childComps/HomeSwiper'
+  import RecommendView from './childComps/RecommendView'
+  import {getHomeMultidata} from 'network/api/home'
+  import FeatureView from './childComps/FeatureView'
 
-  }
-};
+  export default {
+    name: "Home",
+    components: {
+      NavBar,
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+      getHomeMultidata().then(res => {
+        console.log(res)
+        this.banners = res.banner.list
+        this.recommends = res.recommend.list
+      })
+    },
+    methods: {}
+  };
 </script>
 
 <style lang="less" scoped>
-.home-nav {
-  background-color: var(--color-tint);
-  .nav-center {
-    color: #fff;
-  }
-}
+    #home{
+        padding-top: 44px;
+        padding-bottom: 400px;
+    }
+    .home-nav {
+        background-color: var(--color-tint);
+        .nav-center {
+            color: #fff;
+        }
+        position: fixed;
+        left: 0;
+        right: 0;
+        top: 0;
+        z-index: 9;
+    }
+
 </style>
